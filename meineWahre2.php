cd3bb8452a9631c53fc9bf31231defb894370ddc
Uwe Specht <uwe.specht@sap.com>
2021-04-12 17:47:49 +0200

    new files

--- meineWahre2.php
+++ meineWahre2.php
@@ -8,8 +8,8 @@ while ($i < 271) {
     echo "<div class=\"mb-1 pics animation all 1\">\n";
     echo "<a target=\"_blank\" href=\"images/simone/kl$i.jpg\">\n";
     echo "<figure>\n";
-    echo "<img class=\"img_fluid img-thumbnail\" src=\"images/simone/thkl$i.jpg\" alt=\"Card image cap\" title=\"kl$i.jpg\">\n";
-    echo "<figcaption>Kleider kl$i.jpg</figcaption>\n";
+    echo "<img class=\"img-fluid img-thumbnail\" src=\"images/simone/thkl$i.jpg\" alt=\"Card image cap\" title=\"kl$i.jpg\">\n";
+    echo "<figcaption>kl$i.jpg</figcaption>\n";
     echo "</figure>\n";
     echo "</a>\n";
     echo "</div>\n";
@@ -23,12 +23,12 @@ while ($i < 10) {
     $filename = "images/simone/sc$i.jpg";
     if (file_exists($filename)) {
         echo "<div class=\"mb-1 pics animation all 2\">\n";
-        echo "<figure>\n";
         echo "<a target=\"_blank\" href=\"images/simone/sc$i.jpg\">\n";
-        echo "<img class=\"img-fluid\" src=\"images/simone/thsc$i.jpg\" alt=\"Card image cap\" title=\"sc$i.jpg\">\n";
-        echo "<figcaption>Schuhe sc$i.jpg</figcaption>\n";
-        echo "</a>\n";
+        echo "<figure>\n";
+        echo "<img class=\"img-fluid img-thumbnail\" src=\"images/simone/thsc$i.jpg\" alt=\"Card image cap\" title=\"sc$i.jpg\">\n";
+        echo "<figcaption>sc$i.jpg</figcaption>\n";
         echo "</figure>\n";
+        echo "</a>\n";
         echo "</div>\n";
     }
     $i++;
@@ -40,12 +40,12 @@ while ($i < 10) {
     $filename = "images/simone/s$i.jpg";
     if (file_exists($filename)) {
         echo "<div class=\"mb-1 pics animation all 3\">\n";
-        echo "<figure>\n";
         echo "<a target=\"_blank\" href=\"images/simone/s$i.jpg\">\n";
-        echo "<img class=\"img-fluid\" src=\"images/simone/ths$i.jpg\" alt=\"Card image cap\" title=\"s$i.jpg\">\n";
-        echo "<figcaption>Schmuck s$i.jpg</figcaption>\n";
-        echo "</a>\n";
+        echo "<figure>\n";
+        echo "<img class=\"img-fluid img-thumbnail\" src=\"images/simone/ths$i.jpg\" alt=\"Card image cap\" title=\"s$i.jpg\">\n";
+        echo "<figcaption>s$i.jpg</figcaption>\n";
         echo "</figure>\n";
+        echo "</a>\n";
         echo "</div>\n";
     }
     $i++;
@@ -57,12 +57,12 @@ while ($i < 10) {
     $filename = "images/simone/a$i.jpg";
     if (file_exists($filename)) {
         echo "<div class=\"mb-1 pics animation all 4\">\n";
-        echo "<figure>\n";
         echo "<a target=\"_blank\" href=\"images/simone/a$i.jpg\">\n";
-        echo "<img class=\"img-fluid\" src=\"images/simone/tha$i.jpg\" alt=\"Card image cap\" title=\"a$i.jpg\">\n";
-        echo "<figcaption>Accessories a$i.jpg</figcaption>\n";
-        echo "</a>\n";
+        echo "<figure>\n";
+        echo "<img class=\"img-fluid img-thumbnail\" src=\"images/simone/tha$i.jpg\" alt=\"Card image cap\" title=\"a$i.jpg\">\n";
+        echo "<figcaption>a$i.jpg</figcaption>\n";
         echo "</figure>\n";
+        echo "</a>\n";
         echo "</div>\n";
     }
     $i++;
